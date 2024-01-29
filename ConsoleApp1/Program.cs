62327453d4411f5fff80f329fe4d304710cf8963
CardGameInscryption <155000204+CardGameInscryption@users.noreply.github.com>
2024-01-29 20:56:25 +0500

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число 1");
+            double a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число 2");
+            double b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Выберите знак");
+            string vibor_znaka = Console.ReadLine();
+
+            Console.Clear();
+            Console.Write("Ответ: ");
+            if (vibor_znaka == "+")
+            {
+                double result = a + b;
+                Console.WriteLine(result);
+            }
+            else if (vibor_znaka == "-")
+            {
+                double result = a - b;
+                Console.WriteLine(result);
+            }
+            else if (vibor_znaka == "-")
+            {
+                double result = a - b;
+                Console.WriteLine(result);
+            }
+            else if (vibor_znaka == "/")
+            {
+                double result = a / b;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
